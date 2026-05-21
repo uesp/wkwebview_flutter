f20f613f2cac857940068404b46806814bde1fb0
TJ <30050459+Jyndigo@users.noreply.github.com>
2026-05-21 23:10:04 +0100

    m

--- example/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework/Package.swift
+++ example/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework/Package.swift
@@ -1,16 +1,13 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  Generated file. Do not edit.
+// Generated file. Do not edit.
 //
 
 import PackageDescription
 
 let package = Package(
     name: "FlutterFramework",
-    platforms: [
-        .iOS("13.0")
-    ],
     products: [
         .library(name: "FlutterFramework", targets: ["FlutterFramework"])
     ],
--- example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
-//  Generated file. Do not edit.
+// Generated file. Do not edit.
 //
 
 import PackageDescription
@@ -15,11 +15,18 @@ let package = Package(
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        
+        .package(name: "integration_test", path: "../.packages/integration_test"),
+        .package(name: "webview_flutter_wkwebview", path: "../.packages/wkwebview_flutter"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
     ],
     targets: [
         .target(
-            name: "FlutterGeneratedPluginSwiftPackage"
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "integration-test", package: "integration_test"),
+                .product(name: "webview-flutter-wkwebview", package: "webview_flutter_wkwebview"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
         )
     ]
 )
